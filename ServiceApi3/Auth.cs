21dbf29a9f5532559d76c08d0f50021447b27fde
iMCrazyDev <imcrazy.sixtouch@yandex.ru>
2024-04-22 10:18:21 +0300

    Add project files.

---
+++ ServiceApi3/Auth.cs
@@ -0,0 +1,352 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceApi3
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "mircoenv.tech";
+        public const string AUDIENCE = "user";
+        const string KEY = "d3dtneve5max7_1q2w3e4r54tjpotznskpostman888811az";
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+    public class Auth
+    {
+        public static uint GetUserIdByContext(HttpContext httpContext)
+        {
+            var user = httpContext.User;
+            var claim = user.Claims.First();
+            return claim == null ? 0 : uint.Parse(claim.Value);
+        }
+        public static string Hash(string? password, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                                    password: password!,
+                                    salt: salt,
+                                    prf: KeyDerivationPrf.HMACSHA256,
+                                    iterationCount: 100000,
+                                    numBytesRequested: 256 / 8));
+        }
+    }
+    internal class UserRepository
+    {
+        public class UserContext : DbContext
+        {
+            public UserContext()
+            {
+
+            }
+            public DbSet<User> Users { get; set; }
+            public DbSet<Master> Masters { get; set; }
+            public DbSet<Slave> Slaves { get; set; }
+            public DbSet<Access> Accesses { get; set; }
+            public DbSet<SensorData> SensorDatas { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseNpgsql("Host=45.150.64.196;Database=sensor_data;Username=postgres;Password=asdasd@!edqwAD#@wascx"/* 1234"*/, options => options.UseAdminDatabase("postgres"));
+                //optionsBuilder.UseNpgsql("Host=localhost;Database=sensor_data;Username=postgres;Password=1234"/* 1234"*/, options => options.UseAdminDatabase("postgres"));
+            }
+
+            public LoginResult Login(LoginData? data)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(data?.email) || string.IsNullOrEmpty(data?.password))
+                    {
+                        return new (false, "Invalid auth data");
+                    }
+                    User user;
+                    if ((user = Users.FirstOrDefault(x => x.Email == data!.email)) == null)
+                    {
+                        return new (false, "Invalid email");
+                    }
+                    byte[] salt = Convert.FromBase64String(user.Salt!);
+                    string hashed = Auth.Hash(data!.password, salt);
+
+                    if (user.Password != hashed)
+                    {
+                        return new (false, "Invalid password");
+                    }
+
+                    var claims = new List<Claim> { new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()) };
+                    var jwt = new JwtSecurityToken(
+                        issuer: AuthOptions.ISSUER,
+                        audience: AuthOptions.AUDIENCE,
+                        claims: claims,
+                        expires: DateTime.UtcNow.Add(TimeSpan.FromDays(60)),
+                        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+                    return new (true, new JwtSecurityTokenHandler().WriteToken(jwt));
+                }
+                catch { }
+                return new (false, "Error");
+            }
+
+            public RegisterResult Register(RegisterData? data)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(data?.email) || string.IsNullOrEmpty(data?.password) || string.IsNullOrEmpty(data?.name))
+                    {
+                        return new (false, "Invalid register data");
+                    }
+                    if (Users.FirstOrDefault(x => x.Email == data!.email) != null)
+                    {
+                        return new (false, "Email is occupied");
+                    }
+                    byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+                    string hashed = Auth.Hash(data!.password, salt);
+
+                    Users.Add(new User() { Email = data!.email, Password = hashed, Salt = Convert.ToBase64String(salt), Name= data!.name });
+                    SaveChanges();
+
+                    var claims = new List<Claim> { new Claim(JwtRegisteredClaimNames.NameId, Users.FirstOrDefault(x => x.Email == data!.email).Id.ToString()) };
+                    var jwt = new JwtSecurityToken(
+                        issuer: AuthOptions.ISSUER,
+                        audience: AuthOptions.AUDIENCE,
+                        claims: claims,
+                        expires: DateTime.UtcNow.Add(TimeSpan.FromDays(60)),
+                        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+                    return new (true, new JwtSecurityTokenHandler().WriteToken(jwt));
+                }
+                catch { }
+                return new (false, "Error");
+            }
+
+            public User? GetUserById(uint id)
+            {
+                return Users.FirstOrDefault(x => x.Id == id);
+            }
+
+            public string? GetToken(uint master_id, uint user_id)
+            {
+                try
+                {
+                    Master master;
+                    if ((master = Masters.FirstOrDefault(x => x.Id == master_id && x.User.Id == user_id)) == null) {
+                        return "error";
+                    }
+
+                    return master.Token;
+                }
+                catch { }
+                return "error";
+            }
+
+            public (bool, string) CreateMaster(string? name, User? user)
+            {
+                try
+                {
+                    var token = Guid.NewGuid().ToString();
+                    Masters.Add(new Master() { Name = name, User = user, Token = token });
+                    SaveChanges();
+
+                    return (true, token);
+                }
+                catch { }
+                return (false, "Error");
+            }
+
+            public void PushData(Slave slave, string name, int status, double value)
+            {
+                try
+                {
+                    SensorDatas.Add(new SensorData() { Slave = slave, Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(), Value = value, Name = name, Status = status});
+                    SaveChanges();
+                }
+                catch { }
+            }
+
+            public List<SensorData> GetData(long from, long to, uint master_id, uint sensor_id, uint user_id, string name)
+            {
+                var masters = GetMastersByUser(user_id);
+                return SensorDatas.Where(x => x.Name == name && x.Timestamp >= from && to >= x.Timestamp && x.Slave.Master.Id == master_id && x.Slave.Id == sensor_id && (masters.Contains(x.Slave.Master) 
+                    || Accesses.FirstOrDefault(y => x.Slave.Id == y.Slave.Id && y.User.Id == user_id) != null)).ToList();
+            }
+            public List<string> GetDataNames(long from, long to, uint master_id, uint sensor_id, uint user_id)
+            {
+                var masters = GetMastersByUser(user_id);
+                return SensorDatas.Where(x => x.Timestamp >= from && to >= x.Timestamp && x.Slave.Master.Id == master_id && x.Slave.Id == sensor_id && (masters.Contains(x.Slave.Master)
+                    || Accesses.FirstOrDefault(y => x.Slave.Id == y.Slave.Id && y.User.Id == user_id) != null)).GroupBy(w => w.Name).Select(z => z.Key).ToList();
+            }
+
+            public Master? GetMasterByToken(string? token)
+            {
+                return Masters.FirstOrDefault(x => x.Token == token);
+            }
+
+            public List<Master> GetMastersByUser(uint id)
+            {
+                var list = Masters.Where(x => x.User.Id == id).ToList();
+                list.AddRange(Accesses.Where(x => x.User.Id == id && x.AllSlaves).Select(x => x.Master).ToList());
+
+                return list;
+            }
+
+            public void RenameMastersByUser(uint master_id, string new_name)
+            {
+                var master = Masters.FirstOrDefault(x => x.Id == master_id);
+                if (master != null)
+                {
+                    master.Name = new_name;
+                    SaveChanges();
+                }
+            }
+
+            public List<Slave> GetSlavesByUserAndMaster(uint userId, uint masterId)
+            {
+                return Slaves.Where(x => x.Master.Id == masterId && x.Master.User.Id == userId).ToList();
+            }
+
+            public Slave? GetOrCreateSlaveByMasterAndGuid(uint master_id, string guid, string hardware_name)
+            {
+                try
+                {
+                    var slave = Slaves.FirstOrDefault(x => x.Master.Id == master_id && x.Guid == guid);
+
+                    if (slave == null)
+                    {
+                        slave = new Slave()
+                        {
+                            Guid = guid,
+                            HardwareName = hardware_name,
+                            Master = GetMasterById(master_id),
+                            Name = null
+                        };
+                        Slaves.Add(slave);
+                        SaveChanges();
+                    }
+
+                    return slave;
+                }
+                catch { }
+                return null;
+            }
+
+            public Master? GetMasterById(uint master_id)
+            {
+                return Masters.Include(m => m.User).FirstOrDefault(x => x.Id == master_id);
+            }
+
+            public Slave GetSlaveById(uint slave_id)
+            {
+                return Slaves.Include(x => x.Master).FirstOrDefault(x => x.Id == slave_id);
+            }
+
+            public bool RenameSlave(string? newName, Slave slave)
+            {
+                try
+                {
+                    slave.Name = newName;
+                    SaveChanges();
+                    return true;
+                }
+                catch { }
+                return false;
+            }
+        }
+    }
+
+    public class User
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Salt { get; set; }
+        public string? Name { get; set; }
+    }
+
+    [Microsoft.EntityFrameworkCore.Index("Token")]
+    public class Master
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+        public User? User { get; set; }
+        public string? Name { get; set; }
+        public string? Token { get; set; }  //guid
+    }
+
+    public class Slave
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+        public Master? Master { get; set; }
+        public string? Guid { get; set; }
+        public string? Name { get; set; }
+        public string? HardwareName { get; set; }
+
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(Name) ? HardwareName : Name;
+        }
+    }
+
+    public class Access
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+        public Master? Master { get; set; }
+        public User? User { get; set; }
+        public bool AllSlaves { get; set; }
+        public Slave? Slave { get; set; }
+    }
+
+    [Microsoft.EntityFrameworkCore.Index("Timestamp")]
+    public class SensorData
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint Id { get; set; }
+        public long Timestamp { get; set; }
+        public double Value { get; set; }
+        public string? Name { get; set; }
+        public int Status { get; set; }
+        public Slave? Slave { get; set; }
+    }
+    public record LoginData (string email, string password);
+    public record LoginResult (bool result, string details);
+    public record RegisterData(string email, string password, string name);
+    public record RegisterResult(bool result, string details);
+    public record MasterLine(uint id, string name);
+    public record SlaveLine(uint id, string name);
+    public record SensorLine(uint id, long timestamp, double value, int status, string? name);
+    public record GetMasterLine(bool status,string? token);
+    public record PushData(string guid, string hardware_name, SlaveData[] values);
+    public record SlaveData(string sensor_name, int status, SensorOutput[] sensor_data);
+    public record SensorOutput(string sensor_name, double sensor_value);
+    [JsonSerializable(typeof(PushData))]
+    [JsonSerializable(typeof(SlaveData))]
+    [JsonSerializable(typeof(SlaveData[]))]
+    [JsonSerializable(typeof(SensorOutput))]
+    [JsonSerializable(typeof(SensorOutput[]))]
+    [JsonSerializable(typeof(GetMasterLine))]
+    [JsonSerializable(typeof(SensorLine))]
+    [JsonSerializable(typeof(SensorLine[]))]
+    [JsonSerializable(typeof(SlaveLine))]
+    [JsonSerializable(typeof(SlaveLine[]))]
+    [JsonSerializable(typeof(MasterLine))]
+    [JsonSerializable(typeof(MasterLine[]))]
+    [JsonSerializable(typeof(LoginData))]
+    [JsonSerializable(typeof(LoginResult))]
+    [JsonSerializable(typeof(RegisterData))]
+    [JsonSerializable(typeof(RegisterResult))]
+    internal partial class AppJsonSerializerContext : JsonSerializerContext
+    {
+
+    }
+
+
+}
+
---
+++ ServiceApi3/Program.cs
@@ -0,0 +1,278 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.IdentityModel.Tokens;
+using ServiceApi3;
+using static ServiceApi3.UserRepository;
+
+var uc = new UserContext();
+uc.Database.EnsureCreated();
+uc.SaveChanges();
+var builder = WebApplication.CreateSlimBuilder(args);
+builder.WebHost.UseKestrel(kestrelOptions =>
+{
+    kestrelOptions.ListenAnyIP(5000);
+});
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "";// "http://mircroenv.tech";
+        options.Audience = AuthOptions.AUDIENCE;
+        options.ClaimsIssuer = AuthOptions.ISSUER;
+        options.RequireHttpsMetadata = false;
+        options.MetadataAddress = null;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+        };
+    });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader() 
+               .AllowAnyMethod();
+    });
+});
+builder.Services.AddAuthorization();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+
+var userApi = app.MapGroup("/user");
+
+userApi.MapGet("/validate_token", [Authorize] () => Results.Ok());
+
+userApi.MapPost("/login", ([FromBody] LoginData loginData) =>
+{
+    var auth = new UserContext();
+    var res = auth.Login(loginData);
+    return res.result ? Results.Ok(res) : Results.NotFound(res);
+});
+
+userApi.MapPost("/register", ([FromBody] RegisterData registerData) =>
+{
+    var auth = new UserContext();
+    var res = auth.Register(registerData);
+    return res.result ? Results.Ok(res) : Results.NotFound(res);
+});
+
+var masterApi = app.MapGroup("/master");
+
+masterApi.MapGet("/for", [Authorize] (HttpContext context, [FromQuery] uint master_id) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetMasterById(master_id);
+    return res.User.Id == userId ? Results.Ok() : Results.NotFound();
+});
+
+
+masterApi.MapPost("/push", (HttpContext context, [FromQuery] string token, [FromBody] PushData[] registerData) =>
+{
+    var user = new UserContext();
+    var master = user.GetMasterByToken(token);
+    if (master == null) 
+    {
+        return Results.NotFound();
+    }
+
+    foreach (var t in registerData)
+    {
+        try
+        {
+            var guid = t.guid;
+            var slave = user.GetOrCreateSlaveByMasterAndGuid(master.Id, guid, t.hardware_name);
+            foreach (var r in t.values)
+            {
+                foreach (var z in r.sensor_data)
+                {
+                    user.PushData(slave, r.sensor_name + "_" + z.sensor_name, r.status, z.sensor_value);
+                }
+            }
+        }
+        catch { }
+    }
+    return Results.Ok();
+});
+
+/*masterApi.MapPost("/grant_access", (HttpContext context, [FromQuery] string token, [FromBody] PushData registerData) => TODO
+{
+    var user = new UserContext();
+    var res = user.GetMasterByToken(token);
+    if (res == null)
+    {
+        return Results.NotFound();
+    }
+
+
+    return Results.Ok();
+});*/
+
+masterApi.MapGet("/validate_token", (HttpContext context, [FromQuery] string token) =>
+{
+    var user = new UserContext();
+    var res = user.GetMasterByToken(token);
+    return res == null ? Results.NotFound() : Results.Ok();
+});
+
+masterApi.MapGet("/rename", [Authorize] (HttpContext context, [FromQuery] uint master_id, [FromQuery] string name) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetMasterById(master_id);
+    if (res.User.Id == userId)
+    {
+        user.RenameMastersByUser(res.Id, name);
+        return Results.Ok();
+    }
+    return Results.BadRequest();
+});
+
+masterApi.MapGet("/sensor_rename", [Authorize] (HttpContext context, [FromQuery] uint master_id, [FromQuery] uint sensor_id, [FromQuery] string name) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetMasterById(master_id);
+    if (res.User.Id == userId)
+    {
+        var slave = user.GetSlaveById(sensor_id);
+        if (slave.Master.Id == res.Id)
+        {
+            user.RenameSlave(name, slave);
+            return Results.Ok();
+        }
+        return Results.BadRequest();
+    }
+    return Results.BadRequest();
+});
+
+masterApi.MapGet("/token", [Authorize] (HttpContext context, [FromQuery] uint master_id) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetToken(master_id, userId);
+    return res == "error" ? Results.BadRequest(res) : Results.Ok(res);
+});
+
+masterApi.MapGet("/create", [Authorize] (HttpContext context, [FromQuery] string? name) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.CreateMaster(name, user.GetUserById(userId));
+    var resLine = new GetMasterLine(res.Item1, res.Item2);
+    return res.Item1 ? Results.Ok(resLine) : Results.BadRequest(resLine);
+});
+
+masterApi.MapGet("/list", [Authorize] (HttpContext context) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetMastersByUser(userId).Select(x => new MasterLine(x.Id, x.Name));
+    return Results.Ok(res);
+});
+
+
+masterApi.MapGet("/sensors", [Authorize] (HttpContext context, [FromQuery] uint master_id) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetSlavesByUserAndMaster(userId, master_id)
+        .Select(x=> new SlaveLine(x.Id, x.ToString()));
+    return Results.Ok(res);
+});
+
+masterApi.MapGet("/data/names", [Authorize] 
+(HttpContext context, [FromQuery] long from, [FromQuery] long to, [FromQuery] uint master_id, [FromQuery] uint sensor_id) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetDataNames(from, to, master_id, sensor_id, userId);
+    return Results.Ok(res);
+});
+
+masterApi.MapGet("/data", [Authorize] 
+    (HttpContext context, [FromQuery] long from, [FromQuery] long to,
+    [FromQuery] uint master_id, [FromQuery] uint sensor_id, [FromQuery] string name) =>
+{
+    var userId = Auth.GetUserIdByContext(context);
+    if (userId == 0)
+    {
+        return Results.Unauthorized();
+    }
+    var user = new UserContext();
+    var res = user.GetData(from, to, master_id, sensor_id, userId, name)
+    .Select(x => new SensorLine(x.Id, x.Timestamp, x.Value, x.Status, x.Name));
+    return Results.Ok(res);
+});
+
+/*userApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());*/
+
+/*var sampleTodos = new Todo[] {
+    new(1, "Walk the dog"),
+    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+    new(4, "Clean the bathroom"),
+    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+};
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());*/
+
+app.Run();
